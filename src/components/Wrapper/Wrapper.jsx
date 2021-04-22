b4b4aabc881cbcbc3c9ea716f8252ef6a5fed91c
dumaaas <markodumnic8@gmail.com>
2021-04-23 01:55:28 +0200

    Changed backgorund + box shadow

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Wrapper = ({children}) => {
-    return <div style={{display: 'flex', justifyContent: 'center', alignItems:'center', padding: '20px'}}>
+    return <div style={{boxShadow: '0 14px 28px rgb(0 0 0 / 25%), 0 10px 10px rgb(0 0 0 / 22%)', display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems:'flex-start', padding: '20px'}}>
         {children}
     </div>
 }
