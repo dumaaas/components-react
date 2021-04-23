2f0224629a07997dcdde4cb974429d1ec8c34a12
dumaaas <markodumnic8@gmail.com>
2021-04-23 16:34:36 +0200

    Added movie card component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import IdCardFront from './components/IdCardFront/IdCardFront';
+import MovieCard from './components/MovieCard/MovieCard';
 import Wrapper from './components/Wrapper/Wrapper';
 
 function App() {
@@ -10,8 +11,42 @@ function App() {
         <IdCardFront name="Stefan" surname="Tomovic" city="Podgorica" idPhoto="https://dumaaas.github.io/components-react/image/profilePhoto3.png" gender="M/M" citizenship="MNE" birthDate="31.03.1995" idNumber="*********" expire="01.10.2025" signature="Stefan Tomovic"/>
         <IdCardFront name="Milos" surname="Jovovic" city="Budva" idPhoto="https://dumaaas.github.io/components-react/image/profilePhoto2.png" gender="M/M" citizenship="MNE" birthDate="29.09.1995" idNumber="*********" expire="26.04.2027" signature="Milos Jovovic"/>
       </Wrapper>
+      <Wrapper>
+        <MovieCard 
+          name="Interstellar"
+          image="http://localhost:3000/components-react/image/interstellar.jpg" 
+          genre="Adventure, Drama, Sci-Fi" 
+          director="Christopher Nolan" 
+          releaseYear="2014" 
+          actors="Matthew McConaughey, Anne Hathaway, Jessica Chastain" 
+          summary="A group of elderly people are giving interviews about having lived in a climate of crop blight and constant dust reminiscent of The Great Depression of the 1930's. The first one seen is an elderly woman stating her father was a farmer, but did not start out that way."
+          trailer="https://www.youtube.com/watch?v=zSWdZVtXT7E"
+          imdb="https://www.imdb.com/title/tt0816692/"
+        />
+        <MovieCard 
+          name="Fight Club" 
+          image="http://localhost:3000/components-react/image/fightclub.jpg" 
+          genre="Drama" 
+          director="David Fincher" 
+          releaseYear="1999" 
+          actors="Edward Norton, Brad Pitt, Helene Bonham Carter" 
+          summary="A nameless first person narrator (Edward Norton) attends support groups in attempt to subdue his emotional state and relieve his insomniac state. When he meets Marla (Helena Bonham Carter), another fake attendee of support groups, his life seems to become a little more bearable."
+          trailer="https://www.youtube.com/watch?v=qtRKdVHc-cE"
+          imdb="https://www.imdb.com/title/tt0137523/"
+        />
+        <MovieCard 
+          name="Oldboy" 
+          image="http://localhost:3000/components-react/image/oldboy2.jpg" 
+          genre=" Action, Drama, Mystery" 
+          director="Chan-wook Park" 
+          releaseYear="2003" 
+          actors="Choi Min-sik, Yoo Ji-Tae, Kang Hye-jeong" 
+          summary="Dae-Su is an obnoxious drunk bailed from the police station yet again by a friend. However, he's abducted from the street and wakes up in a cell, where he remains for the next 15 years, drugged unconscious when human contact is unavoidable, otherwise with only the television as company."
+          trailer="https://www.youtube.com/watch?v=zSWdZVtXT7E"
+          imdb="https://www.imdb.com/title/tt0364569/"
+        />
+      </Wrapper>
     </div>
-
   );
 }
 
---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import IdPhoto from '../IdPhoto/IdPhoto';
+import './MovieCard.css';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import MovieIcon from '@material-ui/icons/Movie';
+
+const MovieCard = (props) => {
+    return <div className="wrapper-movie">
+        <div className="movie-img">
+            <IdPhoto src={props.image}></IdPhoto>
+        </div>
+        <div className="movie-content">
+            <h1>{props.name} ({props.releaseYear})</h1>
+            <p>{props.director}  / {props.genre}  </p>
+            <h2>SUMMARY</h2>
+            <p>
+                {props.summary}
+            </p>
+            <span>
+                {props.actors}
+            </span>
+            <div className="movie-bottom">
+                <a href={props.trailer} className="trailer" target="_blank">
+                    <PlayArrowIcon/> 
+                    <span>WATCH TRAILER</span> 
+                </a>
+                
+                <a href={props.imdb} className="imdb" target="_blank">
+                    <MovieIcon/> 
+                    <span>IMDb</span> 
+                </a>
+            </div>
+        </div>
+    </div>
+}
+
+export default MovieCard;
